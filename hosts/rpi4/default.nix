133facef26dc6a12e51d69f8d4e0cfc9b257740f
tpdns90321 <tpdns9032100@gmail.com>
2023-07-21 17:55:05 +0900

    폴더 설정 구체화

--- hosts/rpi4/default.nix
+++ hosts/rpi4/default.nix
@@ -17,17 +17,18 @@
       Type = "simple";
       ExecPreStart = ''
         ${pkgs.podman}/bin/podman rm adguardhome >> /dev/null 2>&1 || true
+        mkdir -p /home/${user}/.config/adguardhome/{work,conf} >> /dev/null 2>&1 || true
       '';
       ExecStart = ''
         ${pkgs.podman}/bin/podman run \
           --name adguardhome \
-          --port 5053:53/tcp --port 5053:53/udp \
-          --port 5080:80/tcp --port 5443:443/tcp --port 5443:443/udp --port 8000:3000/tcp \
-          --port 5853:853/tcp \
-          --port 5784:784/udp --port 5853:853/udp --port 13853:8853/udp \
-          --port 5443:10443/tcp \
-          --volume /home/${user}/.config/containers/adguardhome/work:/opt/adguardhome/work \
-          --volume /home/${user}/.config/containers/adguardhome/conf:/opt/adguardhome/conf \
+          --port 6053:53/tcp --port 6053:53/udp \
+          --port 6080:80/tcp --port 6443:443/tcp --port 6443:443/udp --port 9000:3000/tcp \
+          --port 6853:853/tcp \
+          --port 6784:784/udp --port 6853:853/udp --port 14853:8853/udp \
+          --port 11443:5443/tcp \
+          --volume /home/${user}/.config/adguardhome/work:/opt/adguardhome/work \
+          --volume /home/${user}/.config/adguardhome/conf:/opt/adguardhome/conf \
           docker.io/adguard/adguardhome:latest
       '';
       ExecStop = ''
