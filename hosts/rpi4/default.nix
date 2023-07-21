3e283f3f37a07757ea8973c38546d8fbc04f8569
tpdns90321 <tpdns9032100@gmail.com>
2023-07-22 00:35:42 +0900

    발생 오류 해결

--- hosts/rpi4/default.nix
+++ hosts/rpi4/default.nix
@@ -15,27 +15,8 @@
 
     Service = {
       Type = "simple";
-      ExecPreStart = ''
-        ${pkgs.podman}/bin/podman rm adguardhome >> /dev/null 2>&1 || true
-        mkdir -p /home/${user}/.config/adguardhome/{work,conf} >> /dev/null 2>&1 || true
-      '';
-      ExecStart = ''
-        ${pkgs.podman}/bin/podman run \
-          --name adguardhome \
-          --port 6053:53/tcp --port 6053:53/udp \
-          --port 6080:80/tcp --port 6443:443/tcp --port 6443:443/udp --port 9000:3000/tcp \
-          --port 6853:853/tcp \
-          --port 6784:784/udp --port 6853:853/udp --port 14853:8853/udp \
-          --port 11443:5443/tcp \
-          --volume /home/${user}/.config/adguardhome/work:/opt/adguardhome/work \
-          --volume /home/${user}/.config/adguardhome/conf:/opt/adguardhome/conf \
-          docker.io/adguard/adguardhome:latest
-      '';
-      ExecStop = ''
-        ${pkgs.podman}/bin/podman stop adguardhome
-      '';
-      Restart = "always";
-      RestartSec = "30";
+      ExecStart = "${pkgs.podman}/bin/podman run --rm --name adguardhome -p 6053:53/tcp -p 6053:53/udp -p 6080:80/tcp -p 6443:443/tcp -p 6443:443/udp -p 9000:3000/tcp -p 6853:853/tcp -p 6784:784/udp -p 6853:853/udp -p 14853:8853/udp -p 11443:5443/tcp --volume /home/${user}/.config/adguardhome/work:/opt/adguardhome/work --volume /home/${user}/.config/adguardhome/conf:/opt/adguardhome/conf docker.io/adguard/adguardhome:latest";
+      ExecStop = "${pkgs.podman}/bin/podman stop adguardhome";
     };
   };
 }
