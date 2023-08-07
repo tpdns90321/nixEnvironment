d99eb6c9828fc3ebf582a40101c16b61c56c30e0
tpdns90321 <tpdns9032100@gmail.com>
2023-08-07 15:44:11 +0900

    필요한 패키지 추가 및 세부적으로 스크립트 서술

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# requirements
+sudo apt install -y curl lsb-release xz-utils zsh
+
+# default shell change to zsh
+printf "Change default shell to zsh"
+chsh -s $(which zsh)
+
 # systemd-genie
-sudo apt install lsb-release
+printf "Install systemd-genie"
 sudo curl -o /etc/apt/trusted.gpg.d/wsl-transdebian.gpg https://arkane-systems.github.io/wsl-transdebian/apt/wsl-transdebian.gpg
 sudo chmod a+r /etc/apt/trusted.gpg.d/wsl-transdebian.gpg
 cat << EOF | sudo tee /etc/apt/sources.list.d/wsl-transdebian.list
