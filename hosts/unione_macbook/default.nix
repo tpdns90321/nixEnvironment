d96b57c939ddf0785a10b3cd1643602d48a88649
tpdns90321 <tpdns9032100@gmail.com>
2023-08-10 08:52:06 +0900

    사용되는 위치가 변경된 상태라 정정

--- hosts/unione_macbook/default.nix
+++ hosts/unione_macbook/default.nix
@@ -2,7 +2,7 @@
   home-manager.users.${user}.sops = {
     age.keyFile = "/Users/${user}/Library/Application Support/sops/age/keys.txt";
     secrets.openvpn = {
-      sopsFile = ../client.ovpn;
+      sopsFile = ../../client.ovpn;
       format = "binary";
       path = "/Users/${user}/Library/Application Support/TunnelBlick/Configurations/home.ovpn";
     };
