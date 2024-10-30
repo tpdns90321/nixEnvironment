af41f2d62581f7d7034c87379d45e445fd8f663d
tpdns90321 <tpdns9032100@gmail.com>
2024-10-31 00:06:27 +0900

    Some conflict with legacy configuration in Nix, So i disable it and test

--- k3sCluster/cluster.nix
+++ k3sCluster/cluster.nix
@@ -86,11 +86,28 @@ let VIP = "192.168.219.150"; in {
     };
   };
 
-  networking.bridges.br0 = {
-    interfaces = [
-      (if config.networking.hostName == "kang-stay-gmk" then "enp3s0" else "enp34s0") # Replace with your network interface
-      "ztfp6i26fp"
-    ];
+  systemd.network.networks."30-br0-en" = {
+    enable = true;
+
+    matchConfig = {
+      Name = (if config.networking.hostName == "kang-stay-gmk" then "enp3s0" else "enp34s0"); # Replace with your network interface
+    };
+
+    networkConfig = {
+      Bridge = "br0";
+    };
+  };
+
+  systemd.network.networks."30-br0-zt" = {
+    enable = true;
+
+    matchConfig = {
+      Name = "ztfp6i26fp";
+    };
+
+    networkConfig = {
+      Bridge = "br0";
+    };
   };
 
   systemd.services.zerotierone.wantedBy = lib.mkForce [ ];
